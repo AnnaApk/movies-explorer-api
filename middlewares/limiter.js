37beeb5bdde8cca0635dd531bd23a0d1e884e5e3
Anna Apkhanova <anna.karaseva.91@inbox.ru>
2022-09-25 15:57:17 +0300

    fix limiter

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hour
-  max: 30, // Limit each IP to 30 requests per `window` (here, per 1 hour)
+  max: 300, // Limit each IP to 30 requests per `window` (here, per 1 hour)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
